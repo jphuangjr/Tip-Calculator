f8b34cd01808353a6d09ef58d204ae2a37e48ff9
Joshua Huang <jphuangjr@gmail.com>
2015-05-03 15:18:16 -0700

    Initial Commit

---
+++ Tips Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tips Calculator
+//
+//  Created by Joshua Huang on 5/3/15.
+//  Copyright (c) 2015 Joshua Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
