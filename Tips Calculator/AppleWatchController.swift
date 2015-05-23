cbbbd44488fc2bdf52a5086d17e665847e96a5bf
Joshua Huang <jphuangjr@gmail.com>
2015-05-22 17:19:57 -0700

    first commit

--- Tips Calculator/AppleWatchController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppleWatchController.swift
-//  Tips Calculator
-//
-//  Created by Joshua Huang on 5/20/15.
-//  Copyright (c) 2015 Joshua Huang. All rights reserved.
-//
-
-import Foundation
